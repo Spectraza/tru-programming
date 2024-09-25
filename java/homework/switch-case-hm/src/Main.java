2ed9872bc7853df6712d94205563bb5c1797917f
Spectraza <savychvp@gmail.com>
2024-09-24 19:56:11 -0700

    changed a bit

--- java/homework/switch-case-hm/src/Main.java
+++ java/homework/switch-case-hm/src/Main.java
@@ -8,35 +8,27 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter you number: ");
+        System.out.println("Enter you score: ");
         input = scanner.nextInt();
-
-        String letterGrade;
 
         switch (input / 10) {
             case 10:
-                letterGrade = "Your grade is A";
-                System.out.println(letterGrade);
+                System.out.println("Your grade is A");
                 break;
             case 9:
-                letterGrade = "Your grade is A";
-                System.out.println(letterGrade);
+                System.out.println("Your grade is A");
                 break;
             case 8:
-                letterGrade = " Your grade is B";
-                System.out.println(letterGrade);
+                System.out.println("Your grade is B");
                 break;
             case 7:
-                letterGrade = "Your grade is C";
-                System.out.println(letterGrade);
+                System.out.println("Your grade is C");
                 break;
             case 6:
-                letterGrade = "Your grade is D";
-                System.out.println(letterGrade);
+                System.out.println("Your grade is D");
                 break;
             default:
-                letterGrade = "Your grade is F";
-                System.out.println(letterGrade);
+                System.out.println("Your grade is F");
         }
 
     }
